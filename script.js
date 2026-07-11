1fff62b7e2c4da62e77c1f0bbfa3846198907a59
KavyaMC <270106151+KavyaMC@users.noreply.github.com>
2026-07-11 14:54:41 +0530

    feat: implement task creation

--- script.js
+++ script.js
@@ -9,13 +9,42 @@ function updateStatistics() {
         } else {
             activeTasks++;
         }
+    }
         document.getElementById('total-tasks').textContent = totalTasks;
         document.getElementById('active-tasks').textContent = activeTasks;
         document.getElementById('completed-tasks').textContent = totalTasks - activeTasks;
     }
 
-    function createTaskItem(text) { }
-    function handleAddTask(event) { }
+    function createTaskItem(text) {
+        const taskListItem = document.createElement('li');
+        taskListItem.classList.add("list-group-item", "d-flex", "justify-content-between", "align-items-center");
+        const checkBox = document.createElement('input');
+        checkBox.type = 'checkbox';
+        checkBox.classList.add("text-checkbox", "form-check-input", "me-2");
+        const taskText = document.createElement('span');
+        taskText.textContent = text;
+        taskText.classList.add("task-text");
+        const deleteButton = document.createElement('button');
+        deleteButton.textContent = 'Delete';
+        deleteButton.classList.add("delete-btn", "btn", "btn-danger", "btn-sm");
+        taskListItem.append("checkBox", "taskText", "deleteButton");
+        return taskListItem;
+    }
+
+    function handleAddTask(event) {
+        event.preventDefault();
+        const taskInput = document.getElementById('task-input');
+        const taskText = taskInput.value.trim();
+        if (taskText !== '') {
+            const taskList = document.getElementById('task-list');
+            const newTaskItem = createTaskItem(taskText);
+            taskList.appendChild(newTaskItem);
+            taskInput.value = '';
+            taskInput.focus();
+            updateStatistics();
+        }
+    }
+
     function setupFormListener() { }
     function handleTaskClick(event) { }
     function setupTaskListListener() { }
